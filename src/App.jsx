f7cdc6a4652ab341af340b8e9e8d2d8a7226dabc
gexamavetisyan888ait-cyber <gexam.avetisyan888ait@gmail.com>
2026-03-04 22:21:32 +0400

    add in pages Home

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,12 @@ function App() {
 
   return (
     <>
-      
+   
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
