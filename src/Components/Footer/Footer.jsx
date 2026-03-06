421e016f7b93f47d637bc8a463eb959811bc433c
gexamavetisyan888ait-cyber <gexam.avetisyan888ait@gmail.com>
2026-03-06 23:29:15 +0400

    add firebase in designers

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -12,6 +12,7 @@ export default function App() {
 
             <section className="w-full mx-auto px-6 py-5 flex gap-10 h-[500px]">
 
+                
                 <div className="bg-[#0c2430] border border-white/10 rounded-2xl px-5 py-2 shadow-xl w-[80%] ">
                     <h2 className="text-2xl font-semibold mb-2">Կապվեք մեզ հետ</h2>
                     <p className="text-gray-400 text-sm mb-4">
