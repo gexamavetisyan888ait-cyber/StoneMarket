0a34a8ed223b2f853453610da614abceb12f2628
gexamavetisyan888ait-cyber <gexam.avetisyan888ait@gmail.com>
2026-03-06 18:18:06 +0400

    add header and footeer in About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Components/Header/Header'
+import Footer from '../Components/Footer/Footer'
+import About from '../Components/About/About' 
+export default function about() {
+  return (
+    <>
+    <Header/>
+    <About/>
+    <Footer/>
+    </>
+  )
+}
