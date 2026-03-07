ea7756c1459edcc43583471d17c2170510b13177
Gexam Avetisyan <gexam.avetisyan888ait@gmail.com>
2026-03-07 14:02:09 +0400

    add Banner in Components

--- src/Components/Catalog/Banner.jsx
+++ src/Components/Catalog/Banner.jsx
@@ -1,9 +1,6 @@
-import React from "react";
-import Sidebar from "./SideBar";
+import { useRealtimeCollection } from "../../lib/hook";
 
-
-
-function Banner() {
+export default function Banner() {
   return (
     <div className="mt-8 relative w-full rounded-3xl overflow-hidden shadow-lg group">
       <img
@@ -12,23 +9,6 @@ function Banner() {
         alt="Banner"
       />
       <div className="absolute inset-0 bg-black/5 group-hover:bg-black/0 transition-all"></div>
-    </div>
-  );
-}
-
-export default function App() {
-  return (
-    <div className="bg-[#f8f9fa] py-6 px-4 md:px-12">
-      <div className="w-full mx-auto">
-        <div className="flex flex-col lg:flex-row gap-8">
-          <Sidebar />
-          <Hero />
-        </div>
-
-        <div className="w-full">
-          <Banner />
-        </div>
-      </div>
     </div>
   );
 }
